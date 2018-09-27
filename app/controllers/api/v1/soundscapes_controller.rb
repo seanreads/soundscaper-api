9024c98f29a6ce345246698424dede6d6d308ce9
Sean O'Donnell <sean.reads@gmail.com>
2018-09-27 14:50:13 -0400

    wip

--- app/controllers/api/v1/soundscapes_controller.rb
+++ app/controllers/api/v1/soundscapes_controller.rb
@@ -3,8 +3,13 @@ module Api
     class SoundscapesController < ApplicationController
 
       def index
-        @soundscapes = Soundscape.all
-        render json: SoundscapeSerializer.new(@soundscapes).serializable_hash, status: :ok
+        # @soundscapes = Soundscape.all
+        # render json: SoundscapeSerializer.new(@soundscapes).serializable_hash, status: :ok
+        # @locations = ::Api::Base::
+        # ISSUE/SEAN: Should client be initialized once in the application config intializer?
+        # ISSUE/SEAN: Need to cache trend_locations to avoid exceeding Twitter limit
+        client = ::Api::Twitter::Trends.new
+        locations = client.trend_locations
       end
 
       def create
