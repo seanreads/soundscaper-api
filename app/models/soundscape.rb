a12e8fbc21f11285cc56b2345f41d31786b92921
Sean O'Donnell <sean.reads@gmail.com>
2020-11-20 08:46:14 -0500

    async job to create soundscape from trending locations

--- app/models/soundscape.rb
+++ app/models/soundscape.rb
@@ -7,4 +7,27 @@ class Soundscape < ApplicationRecord
     errors.add(:trend_list, "can't be empty") if self.trend_list.empty? 
   end
 
+  def self.create_from_trends_available
+    # ISSUE: Twitter::Error::TooManyRequests: Rate limit exceeded
+    # SOLUTION: Batch them - do ~25 at a time
+    twitter = ::Api::Twitter::Base.new.client
+    locations = twitter.trends_available
+    record_count = 0
+    locations.each do |location|
+      # For a maximum of 25 locations, if location does not yet have a soundscape for
+      # 'today', create a soundscape
+      soundscape = Soundscape.where('name = ? AND created_at > ?', location.name, 1.day.ago)
+      if !soundscape.present?
+        trends = ::Api::Twitter::Formatter.join_trends(twitter.trends(location.woeid))
+        record = Soundscape.create(name: location.name) do |soundscape|
+          soundscape.woeid = location.woeid
+          soundscape.country = location.country
+          soundscape.trend_list = trends
+        end
+        record_count +=1 if record
+        break if record_count >= 25 # TODO: Put maximum record count into config
+      end
+    end    
+  end
+
 end
--- app/workers/soundscape_worker.rb
+++ app/workers/soundscape_worker.rb
@@ -2,17 +2,8 @@ class SoundscapeWorker
   include Sidekiq::Worker
 
   def perform(*args)
-    # TODO/SEAN: Move this into model
-    # ISSUE: Exceeds requests, so move to individually created, user-initiated from UI
-    twitter = ::Api::Twitter::Base.new.client
-    locations = twitter.trend_locations
-    locations.each do |location|
-      trends = ::Api::Twitter::Formatter.join_trends(twitter.trends(location.woeid))
-      Soundscape.create(name: location.name) do |soundscape|
-        soundscape.woeid = location.woeid
-        soundscape.country = location.country
-        soundscape.trend_list = trends
-      end
-    end
+    # For each place with available trends, create a Soundscape record that can be
+    # used to query FreeSound etc and build up supporting assets (audio, images, etc).
+    Soundscape.create_from_trends_available
   end
 end
